a76ccb9a0d99509846461b89196712a9ffadd6c3
Fadhilgva <mfadhilgva12@gmail.com>
2022-07-04 20:10:54 +0700

    Test Commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
             $table->string('password');
+            $table->text('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postalcode')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
