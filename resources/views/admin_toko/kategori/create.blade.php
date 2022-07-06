966cbf7853b167b54637ddb56f7f20e7acee15f4
Yazid <baihaqiyazid16@gmail.com>
2022-07-06 23:15:58 +0700

    Edit label category to name | category store page

--- resources/views/admin_toko/kategori/create.blade.php
+++ resources/views/admin_toko/kategori/create.blade.php
@@ -15,7 +15,7 @@
         </div>
 
         <div class="form-group">
-            <label> Category </label>
+            <label> Name </label>
             <input type="text" name="produk" class="form-control">
         </div>
 
