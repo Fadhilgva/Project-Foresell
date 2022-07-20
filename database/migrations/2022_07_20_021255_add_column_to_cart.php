75c9310ebb17de90c50c6732237a4ff27ff19543
Yazid <baihaqiyazid16@gmail.com>
2022-07-20 09:17:35 +0700

    add store_id to carts

--- database/migrations/2022_07_20_021255_add_column_to_cart.php
+++ database/migrations/2022_07_20_021255_add_column_to_cart.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('cart', function (Blueprint $table) {
+        Schema::table('carts', function (Blueprint $table) {
             $table->integer('store_id')->after('product_id')->nullable();
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('cart', function (Blueprint $table) {
+        Schema::table('carts', function (Blueprint $table) {
             $table->dropColumn('store_id');
         });
     }
