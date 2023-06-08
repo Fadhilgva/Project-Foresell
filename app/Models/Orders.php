2d5c86758822315ade4805d686d7fcfcc3411af2
Fadhilgva <mfadhilgva12@gmail.com>
2023-06-08 08:40:28 +0700

    Fix Store Page

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -16,7 +16,7 @@ class Orders extends Model
     }
     public function Bank()
     {
-        return $this->belongsTo(Payment::class, 'bank_id');
+        return $this->belongsTo(Payment::class, 'bank_id', 'id');
     }
     public function Courier()
     {
